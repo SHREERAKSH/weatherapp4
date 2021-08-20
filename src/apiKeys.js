165a7db270671eb11afa0fd1d0896e0aff6a86e9
SHREERAKSH <shreerakshashetty4@gmail.com>
2021-08-21 00:21:25 +0530

    init

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+    key: "86c0047bcf44c3535d7276b96999c015",
+    base: "https://api.openweathermap.org/data/2.5/",
+  };
